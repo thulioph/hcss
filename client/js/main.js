1b3582b49b612759b52ecd89ac6b6a4942b07786
Thulio Philipe <thulioph@gmail.com>
2018-02-01 20:41:58 -0300

    Updates the client/js with new methods to get techcrunch data.

--- client/js/main.js
+++ client/js/main.js
@@ -1,20 +1,34 @@
-(function() {
+(function () {
     'use strict';
+
+    const techCrunchBtn = document.querySelector('#tech-crunch-button');
+    const techTable = document.querySelector('#techcrunch-table');
+    const techList = document.querySelector('#techcrunch-list');
+
+    const TECHCRUNCH_URL = 'http://localhost:3000/techcrunch';
+    const TECHCRUNCH_RESULT_URL = 'http://localhost:3000/techcrunch/result';
+
+    techCrunchBtn.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        toggleClass(evt.currentTarget);
+        getTechCrunchData();
+    });
+
+    // ====
 
     const hcBtn = document.querySelector('#hacker-news-button');
     const hcList = document.querySelector('#hc-list');
     const hcTable = document.querySelector('#hc-table');
 
-    const HC_URL = 'https://hcss.herokuapp.com/hc';
-    const RESULT_URL = 'https://hcss.herokuapp.com/hc/result';
-
-    // ====
+    const HC_URL = 'http://localhost:3000/hc';
+    const RESULT_URL = 'http://localhost:3000/hc/result';
 
     hcBtn.addEventListener('click', (evt) => {
         evt.preventDefault();
 
         toggleClass(evt.currentTarget);
-        getHackerNewsData()
+        getHackerNewsData();
     });
 
     // ====
@@ -22,7 +36,7 @@
     const toggleClass = (clickedElement) => {
         const elements = document.querySelectorAll('a.mdl-layout__tab');
         elements.forEach(el => el.classList.remove('is-active'));
-        
+
         clickedElement.classList.add('is-active');
     };
 
@@ -57,7 +71,7 @@
 
         const markup = createHackerNewsMarkup(links);
         hcList.innerHTML = markup;
-        
+
         hcTable.classList.remove('is-hidden');
     };
 
@@ -71,6 +85,60 @@
                 </td>
                 <td>${el.author.timestamp}</td>
                 <td>${el.description}</td>
+                <td>
+                    <a target="_blank" href="${el.url}" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                        Link
+                    </a>
+                </td>
+            </tr>
+        `).join('');
+    };
+
+    // ====
+
+    const getTechCrunchData = () => {
+        const request = fetch(TECHCRUNCH_URL, {});
+
+        request.then((data) => {
+            if (data.status === 200) {
+                data.json()
+                    .then((data) => buildTechCrunchData())
+                    .catch((err) => console.error(err))
+            }
+        }).catch((err) => console.error(err))
+    };
+
+    const buildTechCrunchData = () => {
+        const request = fetch(TECHCRUNCH_RESULT_URL, {});
+
+        request.then((data) => {
+            if (data.status === 200) {
+                data.json()
+                    .then((data) => displayTechCrunchData(data))
+                    .catch((err) => console.error(err))
+            }
+        }).catch((err) => console.error(err))
+    };
+
+    const displayTechCrunchData = (obj) => {
+        const { posts, entry } = obj;
+
+        const markup = createTechCrunchMarkup(posts);
+        techList.innerHTML = markup;
+
+        techTable.classList.remove('is-hidden');
+    };
+
+    const createTechCrunchMarkup = (posts) => {
+        return posts.map(el => `
+            <tr>
+                <td>
+                    <a target="_blank" href="${el.author.url}">
+                        ${el.author.name}
+                    </a>
+                </td>
+                <td>${el.timestamp}</td>
+                <td>${el.title}</td>
                 <td>
                     <a target="_blank" href="${el.url}" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
                         Link
